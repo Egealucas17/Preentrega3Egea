4e7aae48045ad4ad6a2986331aed072ed4105f3c
Egealucas17 <egea.lucas@outlook.com>
2023-10-01 00:39:15 -0300

    Se agregaron base e index siguiendo el ejemplo

--- project/config/settings.py
+++ project/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos Aires'
 
 USE_I18N = True
 
