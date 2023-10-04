cf7fdf8e7b9ce07127cacaed62dba765d300cc18
Egealucas17 <egea.lucas@outlook.com>
2023-10-04 20:29:03 -0300

    actualizacion de algunos errores

--- project/alumnos/views.py
+++ project/alumnos/views.py
@@ -8,14 +8,17 @@ def index(request):
 
 
 def cargaAlumno(request):
-    if request.method == "POST":
-        form_alumno = forms.alumnoForm(request.POST)
-        print(form_alumno)
-        if form_alumno.is_valid():
-            info = form_alumno.cleaned_data
-            alumn = models.alumno(info("nombre"), info("escuela"))
-            alumn.save()
-            return render(request, "alumnos/index.html")
+
+        if request.method == "POST":
+            form_alumno = forms.alumnoForm(request.POST)
+
+            print(form_alumno)
+
+            if form_alumno.is_valid():
+                info = form_alumno.cleaned_data
+                alumn = models.alumno(info["nombre"], info["escuela"])
+                alumn.save()
+                return render(request, "alumnos/index.html")
         else: 
             form_alumno = forms.alumnoForm
             return render(request, "alumnos/cargaAlumnoForm.html", {"form_alumno":form_alumno})
