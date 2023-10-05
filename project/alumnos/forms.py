36a0ebd5de84ea45f80e2decb4d4b84741f700ca
Egealucas17 <egea.lucas@outlook.com>
2023-10-05 17:30:17 -0300

    actualizado model de alumnos

--- project/alumnos/forms.py
+++ project/alumnos/forms.py
@@ -6,6 +6,6 @@ from . import models
 class alumnoForm(forms.ModelForm):
     class Meta:
         model = models.alumno
-        fields = ["nombre" , "escuela"]
+        fields = ["nombre" , "escuela", "año", "nivel"]
 
    
--- project/alumnos/models.py
+++ project/alumnos/models.py
@@ -6,6 +6,8 @@ class alumno(models.Model):
 
     nombre = models.CharField(max_length=40)
     escuela = models.CharField(max_length=20)
+    año = models.IntegerField(null= "True" , blank= "true", default= "1")
+    nivel = models.CharField(max_length=10, null= "True" , blank= "true")
 
     
     def __str__(self):
