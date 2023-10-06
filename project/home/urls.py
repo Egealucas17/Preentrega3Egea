a38b6869fea9b25d8eb561ea851c18668edf4014
Egealucas17 <egea.lucas@outlook.com>
2023-10-05 21:14:55 -0300

    arreglado un error en navbar

--- project/home/urls.py
+++ project/home/urls.py
@@ -3,6 +3,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.views.generic import TemplateView
 
+app_name = "home"
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
 
